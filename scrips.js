5c337caaad559fdea333854d809072987eeb140c
andreidumitras <andreei.dumitras@gmail.com>
2025-04-02 20:07:05 +0300

    js code for fetching temperature and humidity

--- scrips.js
+++ scrips.js
@@ -1,11 +1,22 @@
-console.log('Hello, world!');
-function printNumbers(){
-    var number = parseInt(document.getElementById('date-in').value);
-    console.log(number);
-    for (let i = 1; i <= number; i++) {
-        let paragraf = document.getElementById('display');
-        paragraf.style.fontSize = '20px';
-        paragraf.style.color = 'blue';
-        paragraf.innerHTML += i + '<br>';
+async function fetchTemperature() {
+    try {
+        const response = await fetch('http://<your-raspberry-pi-ip>:5000/temperature');
+        const data = await response.json();
+        document.getElementById('temp').innerText = data.temperature;
+        document.getElementById('humi').innerText = data.humidity;
+    } catch (error) {
+        console.error('Error fetching temperature:', error);
     }
 }
+
+setInterval(fetchTemperature, 5000); // Update every 5 sec
+fetchTemperature();
+
+/*
+TO DO:
+ - ngrock pe rpi
+    -> wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip
+    -> unzip ngrok-stable-linux-arm.zip
+
+    onst response = await fetch('https://randomstring.ngrok.io/temperature');
+*/
